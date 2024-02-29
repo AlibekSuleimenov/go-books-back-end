61b48415e144a4067ea3d33d0966cb1b5a0b875e
Alibek Suleimenov <alibek443@gmail.com>
2024-02-29 15:13:15 +0600

    added routes and handlers

---
+++ cmd/api/handlers.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type JSONResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+}
+
+func (app *Application) Login(w http.ResponseWriter, r *http.Request) {
+	type credentials struct {
+		Username string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	var creds credentials
+	var payload JSONResponse
+
+	err := json.NewDecoder(r.Body).Decode(&creds)
+	if err != nil {
+		// send back error message
+		app.ErrorLog.Println("invalid json")
+
+		payload.Error = true
+		payload.Message = "invalid json"
+
+		out, err := json.MarshalIndent(payload, "", "\t")
+		if err != nil {
+			app.ErrorLog.Println(err)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(out)
+		return
+	}
+
+	// TODO authenticate
+	app.InfoLog.Println(creds.Username, creds.Password)
+
+	// send back response
+	payload.Error = false
+	payload.Message = "Signed in"
+
+	out, err := json.MarshalIndent(payload, "", "\t")
+	if err != nil {
+		app.ErrorLog.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
+}
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,23 +1,25 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
 
+// Config
 type Config struct {
 	Port int
 }
 
+// Application
 type Application struct {
 	Config   Config
 	InfoLog  *log.Logger
 	ErrorLog *log.Logger
 }
 
+// main is the main entry point of app
 func main() {
 	var config Config
 	config.Port = 8081
@@ -37,25 +39,14 @@ func main() {
 	}
 }
 
+// Serve starts the web server
 func (app *Application) Serve() error {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		var payload struct {
-			OK      bool   `json:"ok"`
-			Message string `json:"message"`
-		}
-		payload.OK = true
-		payload.Message = "Welcome"
-
-		out, err := json.MarshalIndent(payload, "", "\t")
-		if err != nil {
-			app.ErrorLog.Println(err)
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write(out)
-	})
-
 	app.InfoLog.Println("API listening on port", app.Config.Port)
-	return http.ListenAndServe(fmt.Sprintf(":%d", app.Config.Port), nil)
+
+	serve := &http.Server{
+		Addr:    fmt.Sprintf(":%d", app.Config.Port),
+		Handler: app.routes(),
+	}
+
+	return serve.ListenAndServe()
 }
