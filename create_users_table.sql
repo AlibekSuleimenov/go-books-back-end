eb072fbe32ff0c013dd929e07c934a74d4a0eaff
Alibek Suleimenov <alibek443@gmail.com>
2024-03-01 21:01:58 +0500

    updated sql file, added unique constraint to users table

--- create_users_table.sql
+++ create_users_table.sql
@@ -34,3 +34,6 @@ CREATE SEQUENCE user_id_seq START WITH 1;
 
 ALTER TABLE users
     ALTER COLUMN id SET DEFAULT nextval('user_id_seq');
+
+create unique index users_email_index
+    on public.users (email);
